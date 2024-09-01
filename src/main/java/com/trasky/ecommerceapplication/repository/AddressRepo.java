58e3778afdf29e3ee3c064e33c3f6a5d4fe70fb9
Amala Trasky <amalatrasky15@gmail.com>
2024-09-01 16:05:34 +0530

    Your commit message

--- src/main/java/com/trasky/ecommerceapplication/repository/AddressRepo.java
+++ src/main/java/com/trasky/ecommerceapplication/repository/AddressRepo.java
@@ -4,4 +4,5 @@ import com.trasky.ecommerceapplication.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AddressRepo extends JpaRepository<Address,Long> {
+
 }
